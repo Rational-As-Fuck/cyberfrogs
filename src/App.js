97ef3f041a844b7f0f6f1e2236dea8b63c8c8e16
Holyrood <holyrood@gmail.com>
2022-08-10 19:26:17 -0700

    added second row of NFTs in grid

--- src/App.js
+++ src/App.js
@@ -30,15 +30,36 @@ function App() {
           </Col>
         </Row>
         <Row className="justify-content-center nft-container">
-            <Col className="pod nft">
-              <img src="https://img-cdn.magiceden.dev/rs:fill:640:640:0:0/plain/https://bafybeief7hpj5elx2zpnuljnf5thyemi4mln2bsixuknjibhwkg3vajo4y.ipfs.dweb.link/2665.png?ext=png" alt="NFT"></img>
-            </Col>
-            <Col className="pod nft">
-              <img src="https://img-cdn.magiceden.dev/rs:fill:640:640:0:0/plain/https://bafybeicjadfy7vjxpvzjb3jdzcb2duvi7ojt2scaxpmxbaolw4st54ivk4.ipfs.dweb.link/618.png?ext=png" alt="NFT"></img>
-            </Col>
-            <Col className="pod nft">
-              <img src="https://img-cdn.magiceden.dev/rs:fill:640:640:0:0/plain/https://bafybeifhninzk6cdgsfdxx2vi4matg5hs5aknpnia3f3zijkgio5ybpzfm.ipfs.dweb.link/2338.png?ext=png" alt="NFT"></img>
-            </Col>
+          <Col>
+            <Row className="justify-content-center">
+              <Col className="pod nft">
+                <img src="https://img-cdn.magiceden.dev/rs:fill:640:640:0:0/plain/https://bafybeief7hpj5elx2zpnuljnf5thyemi4mln2bsixuknjibhwkg3vajo4y.ipfs.dweb.link/2665.png?ext=png" alt="NFT"></img>
+              </Col>
+              <Col className="pod nft">
+                <img src="https://img-cdn.magiceden.dev/rs:fill:640:640:0:0/plain/https://bafybeicjadfy7vjxpvzjb3jdzcb2duvi7ojt2scaxpmxbaolw4st54ivk4.ipfs.dweb.link/618.png?ext=png" alt="NFT"></img>
+              </Col>
+              <Col className="pod nft">
+                <img src="https://img-cdn.magiceden.dev/rs:fill:640:640:0:0/plain/https://bafybeifhninzk6cdgsfdxx2vi4matg5hs5aknpnia3f3zijkgio5ybpzfm.ipfs.dweb.link/2338.png?ext=png" alt="NFT"></img>
+              </Col>
+              <Col className="pod nft">
+                <img src="https://img-cdn.magiceden.dev/rs:fill:640:640:0:0/plain/https://bafybeifmi5cjdt4n4zx4fbhqiv3jbeq6bstxvz2ljdl3i5vkfoofblr2da.ipfs.dweb.link/2295.png?ext=png" alt="NFT"></img>
+              </Col>
+            </Row>
+            <Row className="justify-content-center">
+              <Col className="pod nft">
+                <img src="https://img-cdn.magiceden.dev/rs:fill:640:640:0:0/plain/https://bafybeief7hpj5elx2zpnuljnf5thyemi4mln2bsixuknjibhwkg3vajo4y.ipfs.dweb.link/2665.png?ext=png" alt="NFT"></img>
+              </Col>
+              <Col className="pod nft">
+                <img src="https://img-cdn.magiceden.dev/rs:fill:640:640:0:0/plain/https://bafybeicjadfy7vjxpvzjb3jdzcb2duvi7ojt2scaxpmxbaolw4st54ivk4.ipfs.dweb.link/618.png?ext=png" alt="NFT"></img>
+              </Col>
+              <Col className="pod nft">
+                <img src="https://img-cdn.magiceden.dev/rs:fill:640:640:0:0/plain/https://bafybeifhninzk6cdgsfdxx2vi4matg5hs5aknpnia3f3zijkgio5ybpzfm.ipfs.dweb.link/2338.png?ext=png" alt="NFT"></img>
+              </Col>
+              <Col className="pod nft">
+                <img src="https://img-cdn.magiceden.dev/rs:fill:640:640:0:0/plain/https://bafybeifmi5cjdt4n4zx4fbhqiv3jbeq6bstxvz2ljdl3i5vkfoofblr2da.ipfs.dweb.link/2295.png?ext=png" alt="NFT"></img>
+              </Col>
+            </Row>
+          </Col>
         </Row>
       </Container>
     </Container>
